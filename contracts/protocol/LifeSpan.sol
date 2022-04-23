33d0e821ac98ae573c681e30fda00b33a624ea19
Dmitrii <d.mingalev99@gmail.com>
2022-04-23 19:55:20 +0300

    Add PlushForest tests

--- contracts/protocol/LifeSpan.sol
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-// import LifeSpan contract from Plush Protocol
-import "@plushfamily/plush-protocol-contracts/contracts/token/ERC721/LifeSpan.sol";
--- test/all.test.ts
+++ test/all.test.ts
@@ -4,7 +4,6 @@ import { ethers, upgrades } from 'hardhat';
 import { PlushForest, PlushForestController, PlushGetTree } from '../types';
 
 import {
-  LifeSpan,
   Plush,
   PlushAccounts,
   PlushApps,
@@ -18,9 +17,6 @@ describe('Launching the testing of the Plush Studio contracts', () => {
   }
 
   addSigners();
-
-  let LifeSpanFactory: ContractFactory;
-  let lifeSpan: LifeSpan;
 
   let PlushFactory: ContractFactory;
   let plush: Plush;
@@ -45,14 +41,6 @@ describe('Launching the testing of the Plush Studio contracts', () => {
     PlushFactory = await ethers.getContractFactory('Plush');
     plush = (await PlushFactory.deploy()) as Plush;
     await plush.deployed();
-  });
-
-  it('[Deploy contract] Plush Protocol – LifeSpan', async () => {
-    LifeSpanFactory = await ethers.getContractFactory('LifeSpan');
-    lifeSpan = (await upgrades.deployProxy(LifeSpanFactory, {
-      kind: 'uups',
-    })) as LifeSpan;
-    await lifeSpan.deployed();
   });
 
   it('[Deploy contract] Plush Protocol – PlushApps', async () => {
@@ -111,5 +99,78 @@ describe('Launching the testing of the Plush Studio contracts', () => {
       },
     )) as PlushGetTree;
     await plushGetTree.deployed();
+  });
+
+  it('PlushForest -> Check total supply', async () => {
+    expect(await plushForest.totalSupply()).to.eql(ethers.constants.Zero);
+  });
+
+  it('PlushForest -> Checking role assignments', async () => {
+    expect(
+      await plushForest.hasRole(
+        ethers.constants.HashZero,
+        await signers[0].getAddress(),
+      ),
+    ).to.eql(true); // ADMIN role
+    expect(
+      await plushForest.hasRole(
+        '0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6',
+        await signers[0].getAddress(),
+      ),
+    ).to.eql(true); // MINTER role
+    expect(
+      await plushForest.hasRole(
+        '0x65d7a28e3265b37a6474929f336521b332c1681b933f6cb9f3376673440d862a',
+        await signers[0].getAddress(), // PAUSER role
+      ),
+    ).to.eql(true);
+    expect(
+      await plushForest.hasRole(
+        '0x189ab7a9244df0848122154315af71fe140f3db0fe014031783b0946b8c9d2e3',
+        await signers[0].getAddress(), // UPGRADER role
+      ),
+    ).to.eql(true);
+  });
+
+  it('PlushForest -> Checking grant role for PlushGetTree', async () => {
+    const grantMinterRole = await plushForest.grantRole(
+      '0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6', // MINTER role
+      plushGetTree.address,
+    );
+    await grantMinterRole.wait();
+    expect(
+      await plushForest.hasRole(
+        '0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6', // MINTER role
+        plushGetTree.address,
+      ),
+    ).to.eql(true);
+  });
+
+  it('PlushForest -> Check minting from deployer', async () => {
+    const mintToken = await plushForest.safeMint(await signers[0].getAddress());
+    await mintToken.wait();
+    expect(await plushForest.balanceOf(await signers[0].getAddress())).to.eql(
+      ethers.constants.One,
+    );
+    expect(await plushForest.totalSupply()).to.eql(ethers.constants.One);
+  });
+
+  it('PlushForest -> Check pause contract', async () => {
+    const pauseContract = await plushForest.pause();
+    await pauseContract.wait();
+    expect(await plushForest.paused()).to.eql(true);
+    const onpauseContract = await plushForest.unpause();
+    await onpauseContract.wait();
+  });
+
+  it('PlushForest -> Check upgrade contract', async () => {
+    const plushForestNEW = (await upgrades.upgradeProxy(
+      plushForest.address,
+      PlushForestFactory,
+      { kind: 'uups' },
+    )) as PlushForest;
+    await plushForestNEW.deployed();
+    expect(plushForestNEW.address).to.eq(plushForest.address);
+    expect(await plushForest.totalSupply()).to.eql(ethers.constants.One);
   });
 });
